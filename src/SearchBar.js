a590c42eca059101146017d1334751e3f3fe5554
Nayar Joolfoo <nayar@joolfoo.com>
2024-01-11 10:08:03 +0400

    add search bar

---
+++ src/SearchBar.js
@@ -0,0 +1,10 @@
+function SearchBar(){
+    return (
+        <>
+            <input type="text" placeholder="Search..."></input>
+            <button>Search</button>
+        </>
+    )
+}
+
+export default SearchBar
