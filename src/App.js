061b3041879362cbc7d918984eb5881b83a82fb4
Nayar Joolfoo <nayar@joolfoo.com>
2024-01-11 10:50:47 +0400

    made app into class

--- src/App.js
+++ src/App.js
@@ -4,17 +4,32 @@ import Title from './Title';
 import SearchBar from './SearchBar';
 import PresetSearch from './PresetSearch';
 import ImageResults from './ImageResults';
+import React from 'react';
+
 let preselectedwords = ['Mountain','Sea','Sky', 'bird',]
 
-function App() {
-  return (
-    <div>
-      <Title/>
-      <SearchBar/>
-      <PresetSearch preselectedwords={preselectedwords}/>
-      <ImageResults searchtext="Beach"/>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//       <Title/>
+//       <SearchBar/>
+//       <PresetSearch preselectedwords={preselectedwords}/>
+//       <ImageResults searchtext="Beach"/>
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
+  render() {
+    return (
+          <div>
+            <Title/>
+            <SearchBar/>
+            <PresetSearch preselectedwords={preselectedwords}/>
+            <ImageResults searchtext="Beach"/>
+          </div>
+        )
+  }
 }
 
 export default App;
