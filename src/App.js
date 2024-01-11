5e9b374ca2c2cdbdd2d9802b30390307c600cc7a
Nayar Joolfoo <nayar@joolfoo.com>
2024-01-11 10:11:12 +0400

    made preset search

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Title from './Title';
 import SearchBar from './SearchBar';
+import PresetSearch from './PresetSearch';
 
 function App() {
   return (
     <div>
       <Title/>
       <SearchBar/>
+      <PresetSearch/>
     </div>
   );
 }
---
+++ src/PresetSearch.js
@@ -0,0 +1,9 @@
+function PresetSearch() {
+    return (<div>
+        <button>Mountain</button>
+        <button>Beach</button>
+        <button>Birds</button>
+    </div>)
+}
+
+export default PresetSearch
