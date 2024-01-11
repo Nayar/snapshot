d6907003e48fe8f8bc675ffeed3ea01f34ca0dcb
Guilbert21 <guilbertalexis534@gmail.com>
2024-01-11 11:42:18 +0400

    Refactor App component and related components
    Update ImageResults component and remove commented code
    
    Update SearchBar component to handle search functionality

--- src/App.js
+++ src/App.js
@@ -6,31 +6,36 @@ import PresetSearch from './PresetSearch';
 import ImageResults from './ImageResults';
 import React from 'react';
 
-let preselectedwords = ['Mountain','Sea','Sky', 'bird',]
+let preselectedwords = ['Mountain','Sea','Sky', 'bird',];
 
-class App extends React.Component{
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       searchtext: "Sky"
-    }
+    };
   }
 
+  handleChangeSearch = (text) => {
+    this.setState({ searchtext: text });
+  };
+
   changetobeach = (event) => {
-    let text = event.target.outerText
-    this.setState({searchtext: text})
-  }
+    let text = event.target.outerText;
+    this.setState({ searchtext: text });
+  };
 
   render() {
     return (
-          <div>
-            <Title/>
-            <SearchBar/>
-            <PresetSearch preselectedwords={preselectedwords} onclick={this.changetobeach}/>
-            <ImageResults searchtext={this.state.searchtext}/>
-          </div>
-        )
+      <div>
+        <Title />
+        <SearchBar onSearch={this.handleChangeSearch} />
+        <PresetSearch preselectedwords={preselectedwords} onclick={this.changetobeach} />
+        <ImageResults searchtext={this.state.searchtext} />
+      </div>
+    );
   }
 }
 
 export default App;
+
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,10 +1,27 @@
-function SearchBar(){
+import React, { useState } from 'react';
+
+function SearchBar({ onSearch }) {
+    const [searchText, setSearchText] = useState('');
+
+    const handleChange = (event) => {
+        setSearchText(event.target.value);
+    };
+
+    const handleSearch = () => {
+        onSearch(searchText);
+    };
+
     return (
         <>
-            <input type="text" placeholder="Search..."></input>
-            <button>Search</button>
+            <input
+                type="text"
+                placeholder="Search..."
+                value={searchText}
+                onChange={handleChange}
+            />
+            <button onClick={handleSearch}>Search</button>
         </>
-    )
+    );
 }
 
-export default SearchBar
+export default SearchBar;
